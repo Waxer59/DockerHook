2fb5758277a18d0ac79a4a7d54ec08edc484e60a
Hugo Boyano <hnboyano@gmail.com>
2025-03-09 15:22:56 +0100

    fix empty token

--- cmd/dockerHook/main.go
+++ cmd/dockerHook/main.go
@@ -48,28 +48,40 @@ func main() {
 	app.Use(logger.New())
 	app.Use(func(c *fiber.Ctx) error {
 		if !cfg.Auth.Enable {
+			fmt.Println("Authentication disabled")
 			return c.Next()
 		}
 
+		fmt.Println("Authentication enabled, checking token...")
 		registeredTokens := cfg.Auth.Tokens
 		token := c.Query("token")
+
+		if token == "" {
+			fmt.Println("No token provided")
+			return c.SendStatus(fiber.StatusUnauthorized)
+		}
+
 		action := c.Query("action")
 		tokenAccess := cfg.GetTokenActions(token)
 
 		if action == "" {
+			fmt.Printf("No action provided, using default action: %s\n", cfg.Config.DefaultAction)
 			action = cfg.Config.DefaultAction
 		}
 
 		if !slices.Contains(tokenAccess, action) {
+			fmt.Println("Token does not have access to action")
 			return c.SendStatus(fiber.StatusUnauthorized)
 		}
 
 		if !slices.ContainsFunc(registeredTokens, func(t string) bool {
 			return strings.Contains(t, token)
 		}) {
+			fmt.Println("Token is not registered")
 			return c.SendStatus(fiber.StatusUnauthorized)
 		}
 
+		fmt.Println("Token is registered proceeding")
 		return c.Next()
 	})
 
